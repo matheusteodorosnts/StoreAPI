8e4f01c9f00b94f1f266b802a8fe067ac63527e7
matheusteodorosnts <matheusteodorodossantos2013@gmail.com>
2025-07-16 07:48:50 -0300

    docs: Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('StoreAPI')
+    .setDescription('The StoreAPI documentation')
+    .setVersion('1.0')
+    .addTag('store')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
