c06e212b333396371aa4107ccc6672c7aec07787
matheusteodorosnts <matheusteodorodossantos2013@gmail.com>
2025-07-16 07:07:32 -0300

    feat: Add user actions (Update and Delete)

---
+++ src/user/user.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Param, Patch } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { UserDTO } from 'src/auth/DTO/user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private prisma: PrismaService) {}
+
+  @Patch(':iduser/edit')
+  async updateUser(@Param('iduser') iduser: string, @Body() data: UserDTO) {
+    const user = await this.prisma.user.update({
+      where: { id: iduser },
+      data,
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return {
+      user,
+    };
+  }
+
+  @Delete(':iduser/delete')
+  async deleteUser(@Param('iduser') iduser: string) {
+    const user = await this.prisma.user.delete({
+      where: { id: iduser },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return {
+      user,
+    };
+  }
+}
